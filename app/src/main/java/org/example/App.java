35cddf08e903c04c63e5327f0f9d555b3abcf4bd
pb86286 <pb86286@mail.harpercollege.edu>
2025-06-17 15:58:37 -0500

    code

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,36 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
-  public int getInput() {
-    // Implement your function here
+
+  public static int getValidatedInput(String prompt, String errorMessage, int lowerBound, int upperBound) {
+    Scanner scanner = new Scanner(System.in);
+    int userInput;
+
+    while (true) {
+      System.out.println(prompt);
+      if (scanner.hasNextInt()) {
+        userInput = scanner.nextInt();
+        if (userInput >= lowerBound && userInput <= upperBound) {
+          return userInput;
+        } else {
+          System.out.println(errorMessage);
+        }
+      } else {
+        System.out.println(errorMessage);
+        scanner.next(); // clear invalid input
+      }
+    }
   }
 
   public static void main(String[] args) {
-    // Test your function here in a driver program
+    int result = getValidatedInput(
+      "Please enter a value between 1 and 100:",
+      "Your value is invalid",
+      1,
+      100
+    );
+    System.out.println("The value chosen by the user is " + result);
   }
 }
